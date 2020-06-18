cbdf10509ba5015e13788c0d34810d7c46cb0e34
azhry <arliansyah_azhary@yahoo.com>
2020-06-18 13:54:31 +0700

    added tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 module.exports = {
   purge: ['./pages/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'rubik': 'Rubik-Regular',
+        'rubik-bold': 'Rubik-Bold',
+        'rubik-md': 'Rubik-Medium'
+      },
+      fontSize: {
+        'h1:desktop': '50px',
+        'h2:desktop': '35px',
+
+        'h1:mobile': '36px',
+        'h2:mobile': '24px'
+      },
+      colors: {
+        primary: '#ab4693',
+        default: '#4f5665',
+        secondary: '#0b132a',
+        placeholder: '#afb5c0'
+      },
+      backgroundColor: {
+        primary: '#ab4693'
+      },
+      gradients: {
+        'gd-default': ['to top', '#f8f8f8', 'rgba(248, 248, 248, 0)']
+      },
+      borderColor: {
+        default: '#dddddd',
+        active: '#ab4693'
+      },
+      borderRadius: {
+        default: '10px'
+      }
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('tailwindcss-plugins/gradients')
+  ],
 }
