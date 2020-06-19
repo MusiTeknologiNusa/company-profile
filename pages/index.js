40af5abd4583b6ce7eb0d87388add18c9f74306a
Satriadinata <35486119+Penyelidik@users.noreply.github.com>
2020-06-19 18:27:25 +0700

    Highlight & Service Component
    Make Components in Component Folder, and call them @index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import { connect } from 'react-redux';
 import { decrementCounter, incrementCounter } from '~/modules/redux/actions/counterActions';
 import { fetchUsers } from '~/modules/redux/actions/usersActions';
+const Highlight = dynamic(() => import('~/components/Highlight/Highlight'));
+import Highlightpic from '~/assets/images/illustration1.png';
+const ServiceCard = dynamic(() => import('~/components/ServiceCard/ServiceCard'));
+
+import WebImage from '~/assets/images/web.png';
+import AppImage from '~/assets/images/aplikasi.png';
+import MultimediaImage from '~/assets/images/mult.png';
 
 const API_URL = process.env.API_URL;
 
@@ -24,9 +32,25 @@ class App extends React.Component {
 		return (
 			<div>
 				<Navigation />
-				<Highlight />
+				<Highlight image={Highlightpic}/>
 				<Question />
-				<Jobs />
+				<div className="flex flex-col sm:flex-row">
+                    <ServiceCard
+                        label="Website"
+                        width="263"
+                        height="192"
+                        image={WebImage}/>
+                    <ServiceCard
+                        label="Aplikasi"
+                        width="263"
+                        height="192"
+                        image={AppImage}/>
+                    <ServiceCard
+                        label="Multimedia"
+                        width="263"
+                        height="192"
+                        image={MultimediaImage}/>
+                </div>
 				<Whymustus />
 				<History />
 				<ContactUs />
@@ -82,20 +106,6 @@ function Navigation() {
 }
 
 
-function Highlight() {
-	return (
-		<div className="flex md:flex-row-reverse flex-wrap mt-8 ">
-			<div className="w-full sm:w-2/5  p-4  text-gray-200">
-				<img src="illustration1.png" className="w-auto lg:w-11/12 "></img>
-			</div>
-			<div className="w-full text-center sm:w-3/5  p-4 sm:text-left  text-gray-700">
-				<h1 className="main-text inline ml-0 md:ml-20">Solusi Kebutuhan<br></br> <span className="ml-0 md:ml-20">Teknologi dan <span className="md:ml-20 lg:ml-0">Multimedia</span></span></h1>
-				<p className="mt-4 third-text md:ml-20">Jika anda ingin meningkatkan sistem produktiftas anda dengan <br></br>teknologi terkini, mungkin jasa <span className="purple">Muten</span> lah yang anda butuhkan</p>
-				<button className=" hover:bg-red-700 text-white font-bold py-2 px-4 mt-10 primarybutton md:ml-20">Hubungi Kami</button>
-			</div>
-		</div>
-	);
-}
 function Question() {
 	return (
 		<div className="flex mb-4 ml-20 mr-20 md:ml-40">
