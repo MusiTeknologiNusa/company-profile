22d1fe508f0b194003137bdd6790ffbafd50b895
Satriadinata <35486119+Penyelidik@users.noreply.github.com>
2020-06-22 13:50:40 +0700

    fix #24
    fix #24

--- components/Highlight/Highlight.js
+++ components/Highlight/Highlight.js
@@ -8,8 +8,8 @@ export default function(props){
                     <img src={props.image} className="w-auto lg:w-11/12 "></img>
                 </div>
                 <div className="w-full text-center sm:w-3/5  p-4 sm:text-left  text-gray-700">
-                    <h1 className="main-text inline ml-0 md:ml-20">Solusi Kebutuhan<br></br> <span className="ml-0 md:ml-20">Teknologi dan Multimedia</span></h1>
-                    <p className="mt-4 third-text md:ml-20">Jika anda ingin meningkatkan sistem produktiftas anda dengan <br></br>teknologi terkini, mungkin jasa <span className="purple">Muten</span> lah yang anda butuhkan</p>
+                    <h1 className="main-text inline ml-0 md:ml-20">Solusi Kebutuhan<br></br> <span className="ml-0 md:ml-20">Teknologi dan <span className="ml-0 md:ml-20 xl:ml-0"> Multimedia</span></span></h1>
+                    <p className="mt-4 third-text md:ml-20">Jika anda ingin meningkatkan sistem produktiftas dengan <br></br>teknologi terkini, mungkin jasa <span className="purple">Muten</span> lah yang anda butuhkan</p>
                     <button className=" hover:bg-green-500 text-white font-bold py-2 px-4 mt-10 primarybutton md:ml-20">Hubungi Kami</button>
                 </div>
             </div>
--- components/History/History.js
+++ components/History/History.js
@@ -8,7 +8,7 @@ export default function(props) {
         <div className="mt-8">
             <div className="flex mb-4 ml-20 mr-20">
                 <div className="w-full mt-16">
-                    <h2 className="text-center sm:text-center sec-text text-muten">Apa yang Telah Kami <br></br> Kerjakan</h2>
+                    <h2 className="text-center sm:text-center sec-text text-muten">Proyek yang Telah Kami <br></br> Kerjakan</h2>
                     <h2 className="text-center sm:text-center fourth-text mt-8">Ini adalah sebagian dari apa yang telah kami kerjakan dengan berbagai<br></br> macam bidang</h2>
                 </div>
             </div>
